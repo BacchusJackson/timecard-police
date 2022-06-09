8d3ba363c434d01416fc94e4767efcf9d87afa69
Bacchus Jackson <bacchus.jackson@cms.hhs.gov>
2022-06-08 20:27:33 -0500

    Add howdy command

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
+
+@app.command("/howdy")
+async def command(ack, body, respond):
+    await ack()
+    await respond(f"Howdy! <@{body['user_id']}!")
+
+
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
