f05266662f6315da231462a9c4d0a0fe9d16a4c4
Bacchus Jackson <bacchus.jackson@cms.hhs.gov>
2022-06-09 04:44:15 -0500

    Add start command

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ def message_hello(message, say):
     say(f"Howdy <@{message['user']}>!")
 
 
+@app.command("/start")
+def reminders_start(ack, respond, command, body, logger):
+    ack()
+    respond("You've got it! I'll bug you to finish your time card")
+    logger.info(body)
+
+
 if __name__ == "__main__":
     print(f"Logging set to {app.logger.level}")
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
