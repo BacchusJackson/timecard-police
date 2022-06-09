21aa847a4c0e6c22798f8dbb6300478e734eac53
Bacchus Jackson <bacchus.jackson@cms.hhs.gov>
2022-06-09 05:06:54 -0500

    Add simple reminder test

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import datetime
 import logging
 import os
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.errors import SlackApiError
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 logging.basicConfig(level=logging.DEBUG)
+
+channels = []
 
 
 @app.message("hello")
@@ -14,11 +18,28 @@ def message_hello(message, say):
     say(f"Howdy <@{message['user']}>!")
 
 
+@app.message("yes")
+def message_yes(message, say, client):
+    if message["channel_id"] in channels:
+        say("Sweet! I'll leave you alone until tomorrow :smile:")
+        return
+
+
 @app.command("/start")
-def reminders_start(ack, respond, command, body, logger):
+def reminders_start(ack, respond, body, logger, client):
     ack()
-    respond("You've got it! I'll bug you to finish your time card")
-    logger.info(body)
+    try:
+        result = client.chat_scheduleMessage(
+            channel=body["channel_id"],
+            text="Did you finish your time card?",
+            post_at=datetime.date.today() + datetime.timedelta(minutes=1)
+        )
+        logger.info(result)
+        respond("You've got it bud! I'll bug you to finish your time card")
+
+    except SlackApiError as e:
+        respond("Sorry... something went wrong :sad:")
+        logger.error(f"Error scheduling message: {e}")
 
 
 if __name__ == "__main__":
