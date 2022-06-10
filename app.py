f558c801d9f6b4e6d6e303354d3650349538322f
Bacchus Jackson <bacchus.jackson@cms.hhs.gov>
2022-06-10 12:00:27 -0500

    Test sending Async message

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import logging
 import os
@@ -9,11 +10,12 @@ from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 app = AsyncApp(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 logging.basicConfig(level=logging.DEBUG)
+SCHEDULE_FILE = "/data/schedule.yaml"
 
 
 def add_to_schedule(channel_id):
-    logging.debug("Opening schedule.yaml")
-    with open("/data/schedule.yaml", "w+") as file:
+    logging.debug(f"Opening {SCHEDULE_FILE} to add {channel_id} to schedule")
+    with open(SCHEDULE_FILE, "w+") as file:
         doc = yaml.full_load(file)
         logging.debug(doc)
         if doc is None:
@@ -34,26 +36,50 @@ async def message_yes(message, say):
 
 
 @app.command("/start")
-async def reminders_start(ack, respond, body, logger, client, command):
+async def reminders_start(ack, respond, body):
     await ack()
     # scheduled[result["channel"]].append(result["scheduled_message_id"])
     await respond("You've got it bud! I'll make sure you don't forget! :thumbsup:")
     add_to_schedule(body["channel_id"])
 
 
-async def test_func():
-    await asyncio.sleep(2)
-    print("TEST FUNCTION")
+async def scheduler():
+    while True:
+        await asyncio.sleep(3)
+        logging.debug(f"Scheduler Task is opening {SCHEDULE_FILE} to check for channels")
+        if not os.path.exists(SCHEDULE_FILE):
+            logging.debug("schedule.yaml does not exist yet. Skip check schedule")
+            continue
+
+        schedule = []
+        with open(SCHEDULE_FILE, "r") as file:
+            doc = yaml.full_load(file)
+            logging.debug(doc)
+            if doc in None:
+                logging.debug(f"{SCHEDULE_FILE} is None after loading yaml")
+                continue
+            schedule = doc
+
+        for c_id in schedule['channels'].keys():
+            if schedule['channels'][c_id]["done"]:
+                logging.debug(f"Sending a message to {c_id}")
+                result = await app.client.chat_postMessage(
+                    channel=c_id,
+                    text="Hey! Did you complete your time card?"
+                )
+                logging.debug(f"API Result: {result}")
 
 
 async def main():
     handler = AsyncSocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
     task1 = asyncio.create_task(handler.start_async())
-    task2 = asyncio.create_task(test_func())
+    task2 = asyncio.create_task(scheduler())
     await asyncio.gather(task1, task2)
 
 
 if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("INTERRUPTED, SHUTTING DOWN")
+        pass
